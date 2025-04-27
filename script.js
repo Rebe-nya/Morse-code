668802d427b9bbdd70f150f57b772883e0e41ba6
Rebe-nya <filip.slabinaf@gmail.com>
2025-04-27 17:00:49 +0200

    Script of morse code

---
+++ script.js
@@ -0,0 +1,37 @@
+let text = "Čau";
+//let textBezDiakritiky = noDiakr(text);   // "CAU"
+//let morseuvText = morseEncode(textBezDiakritiky);   // "-.-./.-/-.--"
+
+
+
+function morseEncode(noDiakrText){
+    const morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
+    let morseText = "";
+    for (let i = 0; i < noDiakrText.length; i++){
+        morseText += morse[noDiakrText.charCodeAt(i) - 65];
+    }
+    return morseText;
+
+}
+function noDiakr(){
+    const diakrZnaky = "ÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ";
+    const bezDiakrZnaky = "ACDEEINORSTUUYZ";
+    let upperText = text.toUpperCase();
+    let result = "";
+    let output = "";
+    
+    for (let i = 0; i < upperText.length; i++){
+        if (diakrZnaky.indexOf(upperText[i]) !== -1){
+            result = diakrZnaky.indexOf(upperText[i]);
+            result = bezDiakrZnaky[result];
+        } 
+        else {
+            result = upperText[i];
+        }
+        output += result; 
+    }
+    return output;
+}
+
+console.log("noDiakr: " + noDiakr());
+console.log("morseEncode: " + morseEncode(noDiakr()));
